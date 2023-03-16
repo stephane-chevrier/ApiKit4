2d0582b2b21be463bd629ed1d9098f31b180da18
stephane.chevrier <stephane.chevrier@le-campus-numerique.fr>
2023-03-16 13:54:02 +0100

    1er commit

---
+++ src/main/java/campus/apikit4/ApiKit4Application.java
@@ -0,0 +1,13 @@
+package campus.apikit4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKit4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKit4Application.class, args);
+    }
+
+}
---
+++ src/test/java/campus/apikit4/ApiKit4ApplicationTests.java
@@ -0,0 +1,13 @@
+package campus.apikit4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKit4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
